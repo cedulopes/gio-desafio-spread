075dc9df7e2e693f6ea8ecdf6b641b4e205ef1fb
cedulopes <cedulopes@gmail.com>
2021-11-24 16:21:47 -0300

    First Java Program

---
+++ First Java Program/src/com/dio/FirstProgram.java
@@ -0,0 +1,17 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* int a = 1;
+		int b = 2;
+		System.out.println("Hello Word " + (a+b)); */
+		
+		Gato gato = new Gato();
+		System.out.println(gato);
+	}
+
+}
